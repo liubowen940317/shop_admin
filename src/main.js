064deddc519c07731ba7b9b0372197635e0a77ec
liubowen <liubowen@alibaba.com>
2019-06-27 23:22:09 +0800

    登录功能中表单校验完成

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import './assets/css/base.css'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
---
+++ src/views/Login.vue
@@ -0,0 +1,94 @@
+<template>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form
+        ref="form"
+        :rules="formRules"
+        :model="form"
+        class="loginForm"
+        label-position="top"
+        label-width="80px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('form')">登录</el-button>
+          <el-button @click="resetForm('form')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      formRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 5,
+            max: 12,
+            message: "用户名长度必须是5到12个字符",
+            trigger: "change"
+          }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 5,
+            max: 15,
+            message: "密码长度必须是6到15个字符",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+
+  methods: {
+    submitForm(formName) {
+      // this.$refs['loginForm'] 这个就获取到了表单对象
+      // 通过调用这个对象的validate方法，就可以对表单做整体校验
+      // validate函数接收的参数是一个函数
+      this.$refs[formName].validate(valid => {
+        // valid形参，接收到的就是表单的校验结果
+        // 如果表单校验成功则是 true  如果不成功则是false
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.row-bg {
+  height: 100%;
+  background-color: #2d434c;
+}
+.loginForm {
+  padding: 30px 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  min-width: 400px;
+}
+</style>
+
