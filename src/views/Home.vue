1d929b07852d2f339113cd74d8fb070a10f6697f
liubowen <liubowen@alibaba.com>
2019-06-29 21:10:32 +0800

    首页静态页面完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,147 @@
 <template>
-  <div>首页</div>
+  <el-container>
+    <el-header>
+      <el-row type="flex" justify="space-between" align="middle">
+        <el-col :span="6" class="imagelogo">
+          <img src="../assets/logo.png" alt />
+        </el-col>
+        <el-col>
+          <h1 class="header_tit">电商后台管理系统</h1>
+        </el-col>
+        <el-col :span="6">
+          <div class="welcome">
+            欢迎上海前端39期星曜会员
+            <a href="#" class="tc">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :unique-opened="true"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="2-1">
+                <i class="el-icon-menu"></i>
+                <span>角色列表</span>
+              </el-menu-item>
+              <el-menu-item index="2-2">
+                <i class="el-icon-menu"></i>
+                <span>权限列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="3-1">
+                <i class="el-icon-menu"></i>
+                <span>商品列表</span>
+              </el-menu-item>
+              <el-menu-item index="3-2">
+                <i class="el-icon-menu"></i>
+                <span>分类参数</span>
+              </el-menu-item>
+              <el-menu-item index="3-3">
+                <i class="el-icon-menu"></i>
+                <span>商品分类</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="4-1">
+                <i class="el-icon-menu"></i>
+                <span>订单列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="5-1">
+                <i class="el-icon-menu"></i>
+                <span>数据报表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<script>
+export default {
+  methods: {}
+};
+</script>
+
+<style>
+.el-container .el-header {
+  padding: 0;
+  background-color: #b3c1cd;
+}
+.el-col.el-col-6 img {
+  width: 200px;
+}
+.el-header .imagelogo {
+  min-width: 200px;
+}
+.el-header .header_tit {
+  font-size: 28px;
+  color: #fff;
+  min-width: 235px;
+  text-align: center;
+}
+
+.el-header .welcome {
+  min-width: 235px;
+  font-weight: bold;
+  padding-right: 50px;
+}
+.el-header .welcome a {
+  color: #daa520;
+}
+.el-container {
+  height: 100%;
+}
+.el-menu-vertical-demo.el-menu {
+  height: 100%;
+}
+</style>
